9dd8ade842159799075f039ab49b38cc95f03faa
tlabna <tlabna@users.noreply.github.com>
2018-10-04 17:15:54 -0400

    feat: add subscribing to new links
    - add apollo-link-ws & subscriptions-transport-ws to configure apollo
      with subscriptions endpoint
    
    - update index.js:
      - add wsLink function that represents WebSocket connection
      - add link function for proper routing of the requests
        (subscription or query/mutation)
    
    - update LinkList.js:
      - _subscribeToNewLinks function to Query component so that component
        subscribes to events
      - add NEW_LINKS_SUBSCRIPTION subscription definition

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,6 +26,29 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      node {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
 export default class LinkList extends Component {
   _updateCacheAfterVote = (store, createVote, linkId) => {
     // get current state of cached data for FEED_QUERY from the store
@@ -40,12 +63,35 @@ export default class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
+  _subscribeToNewLinks = (subscribeToMore) => {
+    subscribeToMore({
+      // This represents the subscription query itself.
+      // In your case, the subscription will fire every time a new link is created
+      document: NEW_LINKS_SUBSCRIPTION,
+      /* Similar to cache update prop, this function allows you to determine how the store should be updated with the information that was sent by the server after the event occurred */
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink.node
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        })
+      },
+    })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching</div>
           if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
 
           const linksToRender = data.feed.links
 
