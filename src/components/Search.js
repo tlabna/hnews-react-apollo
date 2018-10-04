50dae9420ca3548f2eb05f308868a8f4ec7fa83e
tlabna <tlabna@users.noreply.github.com>
2018-10-04 16:09:38 -0400

    feat: add filtering functionality to Search component
    - update Search.js:
      - add FEED_SEARCH_QUERY definition
      - add _executeSearch function that performs feed query with filter arg

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
 import Link from './Link'
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
 
 class Search extends Component {
   state = {
     links: [],
     filter: '',
+  }
+
+  static propTypes = {
+    client: PropTypes.shape({
+      query: PropTypes.func.isRequired,
+    }).isRequired,
+  }
+
+  _executeSearch = async () => {
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
   }
 
   render() {
@@ -25,10 +65,8 @@ class Search extends Component {
       </div>
     )
   }
-
-  _executeSearch = async () => {
-    // TODO
-  }
 }
 
+// purpose of the withApollo function, it injects the ApolloClient instance
+// that you created in index.js into the Search component as a new prop called client
 export default withApollo(Search)
